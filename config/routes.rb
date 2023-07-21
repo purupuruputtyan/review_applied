1714123cbf4f0df5568762f21ef8343e465427ce
purupuruputtyan <pika0404_minako@yahoo.co.jp>
2023-07-21 13:13:40 +0000

    [Add]header記述追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  get 'homes/about'
+  get 'homes/about', as: 'about'
   devise_for :users
   root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
