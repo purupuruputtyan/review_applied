158578210f90fc01aa1b8e0eb683d9df57c32dc4
purupuruputtyan <pika0404_minako@yahoo.co.jp>
2023-07-22 14:17:47 +0000

    [Add]UsersController記述追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,29 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+      flash[:notice] = "編集に成功しました。"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).premit(:name, :introduction, :profile_image)
   end
 end
