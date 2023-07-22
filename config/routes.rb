8dc3f37895ed518a3fd8a377edaad8aa83765f3f
purupuruputtyan <pika0404_minako@yahoo.co.jp>
2023-07-22 14:08:57 +0000

    [Add]UsersController追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'homes/about', as: 'about'
+
   devise_for :users
+
+  get 'homes/about', as: 'about'
   root to: 'homes#top'
+
+  resources :users, only: [:index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
