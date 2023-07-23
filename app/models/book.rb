d59b8bdf52c2e749b4a82a3086541ae921e2ebb7
purupuruputtyan <pika0404_minako@yahoo.co.jp>
2023-07-23 04:25:34 +0000

    [Add]アソシエーション、バリデーション追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,16 @@
 class Book < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :body, presence: true, length: { maximum: 150 }
+
+  belongs_to :user
+
+  has_one_attached :image
+
+  def get_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_book_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
+  validates :introduction, length: { maximum: 50 }
+  
+  has_many :books
 
   has_one_attached :profile_image
 
