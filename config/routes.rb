18d999df2efe2c22d3e1f8937800012f1b770734
purupuruputtyan <pika0404_minako@yahoo.co.jp>
2023-07-23 04:27:59 +0000

    [Add]booksルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
 
   resources :users, only: [:index, :show, :edit]
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
