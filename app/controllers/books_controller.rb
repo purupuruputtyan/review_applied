ec1a94faf9a5fec2df2567a7bd865ccc98567383
purupuruputtyan <pika0404_minako@yahoo.co.jp>
2023-07-23 04:32:45 +0000

    [Add]booksController記述追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,37 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new
+    if @book.save(book_params)
+      redirect_to books_path
+      flash[:notice] = "投稿に成功しました。"
+    else
+      @books = Book.all
+      render books_path
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def book_params
+    params.require(:book).premit(:title, :body , :image)
   end
 end
