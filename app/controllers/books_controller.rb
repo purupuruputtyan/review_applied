f893aa92fb05530d48b0ac076a91c4d08c5f0a5e
purupuruputtyan <pika0404_minako@yahoo.co.jp>
2023-07-25 02:15:57 +0000

    [Add]投稿機能バグ修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,6 +18,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
     @user = @book.user
   end
 
@@ -36,6 +37,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body , :image)
+    params.require(:book).permit(:title, :body, :image)
   end
 end
